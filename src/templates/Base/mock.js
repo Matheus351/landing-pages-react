b9e6600ea2db010b784443d089c860c165d1ec5d
Matheus351 <matheusniklaus@gmail.com>
2023-03-09 11:35:14 -0300

    adicionando template Base + testes

---
+++ src/templates/Base/mock.js
@@ -0,0 +1,21 @@
+import linksMock from '../../components/NavLink/mock';
+import { GridText } from '../../components/GridText';
+import gridMock from '../../components/GridText/mock';
+
+export const mockBase = {
+  children: (
+    <>
+      <GridText {...gridMock} background />
+      <GridText {...gridMock} />
+      <GridText {...gridMock} background />
+      <GridText {...gridMock} />
+      <GridText {...gridMock} background />
+    </>
+  ),
+  links: linksMock,
+  logoData: {
+    text: 'Logo',
+    link: '#',
+  },
+  footerHtml: '<p>Teste de footer</p>',
+};
